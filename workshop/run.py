fe3e28e855ef5d92f85cc711d9949e47601bdf2a
Abdulmalik Salawu <msalawu5@gmail.com>
2026-01-31 16:40:53 +0100

    initial infra setup - azure backend, encryption, keyvault

--- workshop/run.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from src.app import create_app
-
-env_name = os.getenv('FLASK_ENV', default='development')
-app = create_app(env_name)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
--- workshop/srcpy/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-# initialize db
-db = SQLAlchemy()
